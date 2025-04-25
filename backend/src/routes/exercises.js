78d8c21d19523343b0059ac8b989facdbc9e0669
Omci <melloulchi.omar@gmail.com>
2025-04-25 14:35:21 +0200

    fix

--- backend/src/routes/exercises.js
+++ backend/src/routes/exercises.js
@@ -9,7 +9,7 @@ const pool = new Pool(
       connectionString: process.env.DATABASE_URL,
       ssl: {
         rejectUnauthorized: false,
-        sslmode: 'require'
+        sslmode: 'no-verify'
       }
     }
     : {
